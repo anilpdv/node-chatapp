8841da74bb422b65cfa5938e7b4cbc4e76f55814
anilpdv <pdvanil007@gmail.com>
2019-01-20 12:17:46 +0530

    adding socket io

--- server/server.js
+++ server/server.js
@@ -1,26 +1,37 @@
-var express     = require("express");
-var bodyParser  = require("body-parser");
-var mongoose    = require("mongoose");
-var path        = require("path");
-var socketIO    = require("socket.io");
-var http        = require("http");
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var path = require("path");
+var socketIO = require("socket.io");
+var http = require("http");
 // path
-const publicPath = path.join(__dirname,"../public");
+const publicPath = path.join(__dirname, "../public");
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
 // configure
 app.use(express.static(publicPath));
+
+// connectio
+io.on("connection", socket => {
+  console.log("new user connected");
+  socket.on("disconnect", () => {
+    console.log("client disconnect");
+  });
+});
+
 var port = process.env.PORT || 3000;
 // listen to port:3000
-app.listen(port,()=>{
-    console.log("[chat code app] app is running on port: " +port);
-    setTimeout(function(){
+server.listen(port, () => {
+  console.log("[chat code app] app is running on port: " + port);
+  setTimeout(function() {
     console.log("[chat code app] app started...");
-    },2000);
-    setTimeout(function(){
-        console.log("[chat code app] happy coding!\(.-.)/");
-    },1000);
-    setTimeout(function(){
+  }, 2000);
+  setTimeout(function() {
+    console.log("[chat code app] happy coding!(.-.)/");
+  }, 1000);
+  setTimeout(function() {
     console.log("[chat code app] click here https://localhost:3000");
-    },3000);
+  }, 3000);
 });
